08a213c0c07bea7c4186d38337e5705b6b3cb22c
AndreyTrubitsyn <iverstoon@icloud.com>
2021-11-11 11:09:43 +0400

    init new branch

--- src/modules/character/index.tsx
+++ src/modules/character/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 export const CharacterScreen = () => {
   return (
     <View>
-      <Text>Character</Text>
+      <Text>Character Morty</Text>
     </View>
   )
 }
